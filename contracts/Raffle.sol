815eabab49fed8734bf7704475b518c8f76f1d92
lapatric <42653152+lapatric@users.noreply.github.com>
2022-08-01 18:25:12 +0200

    integrated Chainlink VRF v2

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,17 +9,35 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+error Raffle__TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery Variables
+    address private s_recentWinner;
 
     /* Events */
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
-    constructor(address vrfCoorinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoorinatorV2){
+    constructor(address vrfCoordinatorV2, uint256 entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -36,14 +54,30 @@ contract Raffle is VRFConsumerBaseV2 {
         // Request the random number
         // Do something with it
         // 2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gasLane - max gas you're willing to pay for request
+            i_subscriptionId, // given to us from the subscription contract we use to pay the oracle gas
+            REQUEST_CONFIRMATIONS, // how many confirmations before we receive generated random number
+            i_callbackGasLimit, // gas limit on the fulfillRandomWords callback function
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
     /* 
         Overrides the (virtual) fulfillRandomWords function in node_modules/@chainlink/src/v0.8/VRFConsumerBaseV2 
         which we must do as we inheret VRFConsumerBaseV2
     */
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-
+    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        // require(success);
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
     }
 
     /* View / Pure functions */
@@ -53,6 +87,10 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns(address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns(address) {
+        return s_recentWinner;
     }
 
 }
